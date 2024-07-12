d4e2b596c86932302a2eab7df52ff86f1010955b
Eric Scouten <scouten@adobe.com>
2024-07-11 18:37:18 -0700

    Take settings out of public API surface

--- src/lib.rs
+++ src/lib.rs
@@ -37,7 +37,7 @@ pub mod cose_sign;
 #[cfg(feature = "openssl_sign")]
 pub mod create_signer;
 pub(crate) mod jumbf_io;
-pub mod settings;
+pub(crate) mod settings;
 pub mod validation_status;
 #[cfg(target_arch = "wasm32")]
 pub mod wasm;
