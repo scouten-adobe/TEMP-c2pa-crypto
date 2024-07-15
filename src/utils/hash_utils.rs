f292a88cb454133377e5fa25b4b80a4adad82d81
Eric Scouten <scouten@adobe.com>
2024-07-15 07:42:31 -0700

    Move Hasher over to WASM specific code

--- src/utils/hash_utils.rs
+++ src/utils/hash_utils.rs
@@ -15,8 +15,6 @@
 use multibase::{decode, encode};
 use multihash::{wrap, Code, Multihash, Sha1, Sha2_256, Sha2_512, Sha3_256, Sha3_384, Sha3_512};
 use serde::{Deserialize, Serialize};
-// direct sha functions
-use sha2::{Digest, Sha256, Sha384, Sha512};
 
 const MAX_HASH_BUF: usize = 256 * 1024 * 1024; // cap memory usage to 256MB
 
@@ -72,37 +70,6 @@ pub(crate) fn hash_by_type(hash_type: u8, data: &[u8]) -> Option<Multihash> {
         0x15 => Some(Sha3_384::digest(data)),
         0x16 => Some(Sha3_256::digest(data)),
         _ => None,
-    }
-}
-
-#[derive(Clone)]
-pub(crate) enum Hasher {
-    SHA256(Sha256),
-    SHA384(Sha384),
-    SHA512(Sha512),
-}
-
-impl Hasher {
-    // update hash value with new data
-    pub(crate) fn update(&mut self, data: &[u8]) {
-        use Hasher::*;
-        // update the hash
-        match self {
-            SHA256(ref mut d) => d.update(data),
-            SHA384(ref mut d) => d.update(data),
-            SHA512(ref mut d) => d.update(data),
-        }
-    }
-
-    // consume hasher and return the final digest
-    pub(crate) fn finalize(hasher_enum: Hasher) -> Vec<u8> {
-        use Hasher::*;
-        // return the hash
-        match hasher_enum {
-            SHA256(d) => d.finalize().to_vec(),
-            SHA384(d) => d.finalize().to_vec(),
-            SHA512(d) => d.finalize().to_vec(),
-        }
     }
 }
 
--- src/wasm/hash_by_alg.rs
+++ src/wasm/hash_by_alg.rs
@@ -20,6 +20,37 @@ use sha2::{Digest, Sha256, Sha384, Sha512};
 
 use crate::Result;
 
+#[derive(Clone)]
+pub(crate) enum Hasher {
+    SHA256(Sha256),
+    SHA384(Sha384),
+    SHA512(Sha512),
+}
+
+impl Hasher {
+    // update hash value with new data
+    pub(crate) fn update(&mut self, data: &[u8]) {
+        use Hasher::*;
+        // update the hash
+        match self {
+            SHA256(ref mut d) => d.update(data),
+            SHA384(ref mut d) => d.update(data),
+            SHA512(ref mut d) => d.update(data),
+        }
+    }
+
+    // consume hasher and return the final digest
+    pub(crate) fn finalize(hasher_enum: Hasher) -> Vec<u8> {
+        use Hasher::*;
+        // return the hash
+        match hasher_enum {
+            SHA256(d) => d.finalize().to_vec(),
+            SHA384(d) => d.finalize().to_vec(),
+            SHA512(d) => d.finalize().to_vec(),
+        }
+    }
+}
+
 const MAX_HASH_BUF: usize = 256 * 1024 * 1024; // cap memory usage to 256MB
 
 pub(crate) fn hash_by_alg(alg: &str, data: &[u8]) -> Vec<u8> {
