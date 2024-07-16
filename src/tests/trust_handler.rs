729c12b3f6658bd0fd6ce79f1655e10ad75898f3
Eric Scouten <scouten@adobe.com>
2024-07-16 11:09:30 -0700

    Test coverage for trust_handler::load_trust_from_data

--- src/tests/trust_handler.rs
+++ src/tests/trust_handler.rs
@@ -50,3 +50,45 @@ mod load_eku_configuration {
         assert!(eku_iter.next().is_none());
     }
 }
+
+#[cfg(feature = "openssl_sign")] // TEMPORARY until temp_signer is generic
+mod load_trust_from_data {
+    use crate::{
+        openssl::temp_signer, trust_handler::load_trust_from_data, Error, Signer, SigningAlg,
+    };
+
+    #[test]
+    fn allowed_list_pem() {
+        let allowed_list = include_bytes!("fixtures/allow_list/allowed_list.pem").to_vec();
+        let allowed_list = load_trust_from_data(&allowed_list).unwrap();
+
+        let not_a_cert = b"bogus".to_vec();
+        assert!(!allowed_list.contains(&not_a_cert));
+
+        let ps256 = temp_signer::get_rsa_signer(SigningAlg::Ps256, None);
+        let ps256_certs = ps256.certs().unwrap();
+        assert!(allowed_list.contains(&ps256_certs[0].to_owned()));
+
+        let ps384 = temp_signer::get_rsa_signer(SigningAlg::Ps384, None);
+        let ps384_certs = ps384.certs().unwrap();
+        assert!(allowed_list.contains(&ps384_certs[0].to_owned()));
+
+        let ps512 = temp_signer::get_rsa_signer(SigningAlg::Ps512, None);
+        let ps512_certs = ps512.certs().unwrap();
+        assert!(allowed_list.contains(&ps512_certs[0].to_owned()));
+    }
+
+    #[test]
+    fn errors_on_invalid_pem() {
+        let allowed_list =
+            b"-----BEGIN CERTIFICATE-----\nnot a valid certificate\n-----END CERTIFICATE-----\n"
+                .to_vec();
+        let err = load_trust_from_data(&allowed_list).unwrap_err();
+        match err {
+            Error::CoseInvalidCert => (),
+            _ => {
+                panic!("Unexpected error: {err:#?}");
+            }
+        }
+    }
+}
