8a801152f5d3af7508ecc40818d6e1cd99ad1515
Eric Scouten <scouten@adobe.com>
2024-07-15 21:00:26 -0700

    Remove status_tracker::{report_has_status, report_has_err}

--- src/status_tracker.rs
+++ src/status_tracker.rs
@@ -194,34 +194,8 @@ impl StatusTracker for OneShotStatusTracker {
         self.logged_items.push(log_item);
     }
 }
-/// Check to see if report contains a specific C2PA status code
-#[allow(dead_code)] // in case we make use of these or export this
-pub fn report_has_status(report: &[LogItem], val: &str) -> bool {
-    report.iter().any(|vi| {
-        if let Some(vs) = &vi.validation_status {
-            vs == val
-        } else {
-            false
-        }
-    })
-}
 
-/// Check to see if report contains a specific error
-#[allow(dead_code)] // in case we make use of these or export this
-pub fn report_has_err(report: &[LogItem], err: Error) -> bool {
-    let err_type = format!("{:?}", &err);
-    report.iter().any(|vi| {
-        if let Some(e) = &vi.err_val {
-            e == &err_type
-        } else {
-            false
-        }
-    })
-}
-
-/// Split Errors off from rest of report
-#[allow(dead_code)] // in case we make use of these or export this
-pub fn report_split_errors(report: &mut Vec<LogItem>) -> Vec<LogItem> {
+pub(crate) fn report_split_errors(report: &mut Vec<LogItem>) -> Vec<LogItem> {
     let mut output: Vec<LogItem> = Vec::new();
 
     let mut i = 0;
